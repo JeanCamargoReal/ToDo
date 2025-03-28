c4be25ef997ac379d41af9863f432a5874e64199
Jean Camargo <jeancamargoreal@icloud.com>
2025-03-28 09:56:45 -0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Jean Camargo on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
